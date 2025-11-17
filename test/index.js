c79f6e333682fc8311e3a853f37806f0b1e2d926
Kidus Geremew <142576188+kidusGIT@users.noreply.github.com>
2025-11-17 20:44:14 +0300

    Add initial GSAP animation setup with HTML and JavaScript files
    - Created index.html with a basic layout and buttons for controlling animations.
    - Implemented index.js to define a GSAP timeline for animating colored balls.
    - Added event listeners for play, pause, reverse, and resume functionalities.

---
+++ test/index.js
@@ -0,0 +1,35 @@
+import { gsap } from "../src/index.js";
+
+const start = document.getElementById("btn-start");
+const pause = document.getElementById("btn-pause");
+const reverse = document.getElementById("btn-reverse");
+const play = document.getElementById("btn-play");
+const resume = document.getElementById("btn-resume");
+
+const t = gsap.timeline();
+t.to(".green", { duration: 1, x: 535 })
+  .to(".yellow", { duration: 2, x: 535 }, ">-2")
+  .to(".red", { duration: 1, x: 535 }, "<-1");
+
+// const t = tween
+// (".yellow", "left", { duration: 1000, to: 420 });
+
+pause.onclick = () => {
+  t.pause();
+};
+
+reverse.onclick = () => {
+  t.reverse();
+};
+
+play.onclick = () => {
+  t.play();
+};
+
+resume.onclick = () => {
+  t.resume();
+};
+
+// start.onclick = () => {
+//   // t.animate(".yellow", "left", { duration: 1000, to: 420 });
+// };
