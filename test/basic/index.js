11eda6c4a9b5295baea7e19e55240ce10f629de6
Kidus Geremew <kidusg98@gmail.com>
2025-12-25 15:29:00 +0300

    add initial HTML and JavaScript setup for GSAP animations

--- test/basic/index.js
+++ test/basic/index.js
@@ -1,4 +1,4 @@
-import { gsap } from "../src/index.js";
+import { gsap } from "../../src/index.js";
 
 const start = document.getElementById("btn-start");
 const pause = document.getElementById("btn-pause");
@@ -6,17 +6,22 @@ const reverse = document.getElementById("btn-reverse");
 const play = document.getElementById("btn-play");
 const resume = document.getElementById("btn-resume");
 
-// const t = gsap.timeline();
-// t.to(".green", { duration: 1, x: 535 })
-//   .to(".yellow", { duration: 2, x: 535 }, ">-2")
-//   .to(".red", { duration: 1, x: 535 }, "<-1");
-
 const t = gsap.timeline();
 t.to(".green", { duration: 1, x: 535 })
-  .to(".yellow", { duration: 2, x: 535 })
-  .to(".red", { duration: 1, x: 535 });
+  .to(".yellow", { duration: 2, x: 535 }, ">-2")
+  .to(".red", { duration: 1, x: 535 }, "<-1");
 
-// const t = gsap.to(".green", { duration: 1.5, x: 535 });
+// const t = gsap.timeline();
+// t.to(".green", { duration: 1, x: 535 })
+//   .to(".yellow", { duration: 2, x: 535 })
+//   .to(".red", { duration: 1, x: 535 });
+
+// const t = gsap.to(".green", {
+//   duration: 1.5,
+//   x: 535,
+//   // rotation: 360,
+//   // repeat: -1,
+// });
 
 pause.onclick = () => {
   t.pause();
