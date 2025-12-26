5727419536a90b8302b21c59e860e416c44a10a4
Kidus Geremew <kidusg98@gmail.com>
2025-12-26 18:02:07 +0300

    adding test for scroll

--- test/scroll/index.js
+++ test/scroll/index.js
@@ -2,19 +2,45 @@ import { gsap } from "../../src/index.js";
 import { ScrollTrigger } from "../../src/ScrollTrigger.js";
 gsap.registerPlugin(ScrollTrigger);
 
-const t = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".card",
-    start: "-100px center",
-    end: "300px center",
-    scrub: true,
-    markers: true,
-    // toggleActions: 'onEnter onLeave onEnterBack onLeaveBack' // make scrub false
-  },
-});
+// const t = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: [".violet", ".blue"], // for multiple targets
+//     start: "-100px 40%",
+//     end: "300px 50%",
+//     scrub: true,
+//     markers: true,
+//     // toggleActions: 'onEnter onLeave onEnterBack onLeaveBack' // make scrub false
+//   },
+// });
+
+// t.to(".violet", {
+//   x: 1100,
+// }).to(".blue", {
+//   x: 900,
+// });
 
 // onEnter onLeave onEnterBack onLeaveBack can be [ play, pause, reverse, resume, restart, reset, complete, none ]
 
-t.to(".card", {
+gsap.to(".violet", {
   x: 1100,
+  scrollTrigger: {
+    trigger: ".violet", // for multiple targets
+    start: "-50px 20%",
+    end: "300px 50%",
+    scrub: true,
+    markers: true,
+  },
+});
+
+gsap.to(".blue", {
+  x: 900,
+  scrollTrigger: {
+    trigger: ".blue", // for multiple targets
+    // start: "-50px 70%",
+    // end: "300px 90%",
+    start: "-50px 20%",
+    end: "300px 50%",
+    scrub: true,
+    markers: true,
+  },
 });
