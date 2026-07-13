414e51ca7730571e622e270f984a43d9c418e17a
Kidus Geremew <kidusg98@gmail.com>
2026-07-13 16:48:39 +0300

    Add SVG animation controls with play, pause, reverse, resume, and seek buttons
    - Updated svg-index.html to include buttons for controlling SVG animations.
    - Styled buttons with CSS for better appearance and layout.
    - Implemented button functionality in svg-index.js to control GSAP animation timeline.

--- test/svg/simple/svg-index.js
+++ test/svg/simple/svg-index.js
@@ -3,4 +3,33 @@ import { MorphSVGPlugin } from "../../../src/MorphSVGPlugin.js";
 
 gsap.registerPlugin(MorphSVGPlugin);
 
-let tl = gsap.to("#diamond", { morphSVG: "#lightning" });
+const start = document.getElementById("btn-start");
+const pause = document.getElementById("btn-pause");
+const reverse = document.getElementById("btn-reverse");
+const play = document.getElementById("btn-play");
+const resume = document.getElementById("btn-resume");
+const seek = document.getElementById("btn-seek");
+
+let t;
+t = gsap.to("#diamond", { morphSVG: "#lightning", duration: 1.5 });
+
+pause.onclick = () => {
+  t.pause();
+};
+
+reverse.onclick = () => {
+  console.log("-----------------------------------");
+  t.reverse();
+};
+
+play.onclick = () => {
+  t.play();
+};
+
+resume.onclick = () => {
+  t.resume();
+};
+
+seek.onclick = () => {
+  t.seek(1.5);
+};
